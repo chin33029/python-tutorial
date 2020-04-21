298ba01d06f652812986a8757f2e8f160ca5fc75
rickychin <chin33029@yahoo.com>
2020-04-20 21:09:08 -0400

    push assignment 2

--- assignments/assignment_02.py
+++ assignments/assignment_02.py
@@ -1,38 +1,50 @@
-""" Assignment 02 - Bank Example """
+"""Bank Assignment"""
 
 
-def deposit(amount, balance):
-    """ Deposit money to account """
-    # <Enter code here to return the new balance>
+BEGINING = 100
 
 
-def withdraw(amount, balance):
-    """ Withdraw money from account """
-    # <Enter code here to return the new balance>
-    # Hint: You should also check if user has enough
-    #       enough money for the withdraw
+def say_hello(BEGINING):
+    print('Welcome To First Python Bank')
+    print(f'Your Current Balance Is {BEGINING}')
+      
+
+def dep(dep_amt, BEGINING,):
+    return int(dep_amt) + BEGINING
+       
+
+def withdraw(with_amt, BEGINING):
+    if int(with_amt) < int(BEGINING):
+       return BEGINING - int(with_amt) 
+    else: 
+       print(f'Unable to withdraw {with_amt}, you only have {BEGINING}')
+    return BEGINING
 
 
-if __name__ == "__main__":
-    # Set the beginning balance
-    BALANCE = 100
-
-    # Welcome the customer
-
-    # Tell the user their current balance
-
-    # Ask the customer what action do they want to take.
-    # Uppercase the response to make it easier for comparisons
-
-    # process action if the user entered a valid action (deposit/withdraw)
-    # <if statement goes here>
-        # Ask the user how much
-
-        # Process the transaction
 
 
-        # Tell the user their new balance
 
-    # Tell the user that the entered action is not valid
-    # <else statement goes here>
-        # print message to user
+if __name__ == '__main__':
+
+    say_hello(BEGINING)
+customer_option = input('What would you like to do(Deposit, Withdraw, Exit)?')
+customer_input = customer_option.lower().strip('')
+chk_val = 0
+
+if customer_input == 'deposit':
+    dep_amt = input('How much would you like to deposit?')
+    chk_val = int(chk_val) + 1
+    print('Your Balance is Now ' + str(dep(dep_amt, BEGINING)))
+           
+if customer_input == 'withdraw':
+    with_amt = input('How much would you like to withdraw?')
+    chk_val = int(chk_val) + 1 
+    print('Your Balance is Now ' + str(withdraw(with_amt, BEGINING)))
+
+if customer_input == 'exit':
+    chk_val = int(chk_val) + 1
+    print(f'Your Balance is {BEGINING} ')
+    print('Have a Nice Day!')
+if chk_val == 0:
+    print(f'Sorry, {customer_option} is an invalid action.')
+    
